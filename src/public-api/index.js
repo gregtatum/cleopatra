87c8227abd9b3fe43db77269d80cd092c2f7c5d0
Greg Tatum <tatum.creative@gmail.com>
2017-07-23 15:00:46 -0500

    Modify API

--- src/public-api/index.js
+++ src/public-api/index.js
@@ -3,7 +3,41 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 // @flow
-import { unserializeProfileOfArbitraryFormat as processProfile } from '../profile-logic/process-profile';
+import { unserializeProfileOfArbitraryFormat } from '../profile-logic/process-profile';
 import { getCallTree } from '../profile-logic/profile-tree';
+import {
+  getFuncStackInfo,
+  getFriendlyThreadName,
+} from '../profile-logic/profile-data';
 
-export { processProfile, getCallTree };
+import type { Profile, Thread } from '../types/profile';
+import type { FuncStackInfo } from '../types/profile-derived';
+import type { ImplementationFilter } from '../types/actions';
+import type { ProfileTreeClass as ProfileTree } from '../profile-logic/profile-tree';
+
+type CallTreeOptions = {
+  invert?: boolean,
+  funcStackInfo?: FuncStackInfo,
+  implementationFilter?: ImplementationFilter,
+};
+
+function getCallTreePublic(
+  profile: Profile,
+  thread: Thread,
+  options?: CallTreeOptions = {}
+): ProfileTree {
+  return getCallTree(
+    thread,
+    profile.meta.interval,
+    options.funcStackInfo ||
+      getFuncStackInfo(thread.stackTable, thread.frameTable, thread.funcTable),
+    options.implementationFilter || 'combined',
+    options.invert || false
+  );
+}
+
+export {
+  unserializeProfileOfArbitraryFormat as processProfile,
+  getCallTreePublic as getCallTree,
+  getFriendlyThreadName,
+};
